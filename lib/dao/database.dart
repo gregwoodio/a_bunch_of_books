c489ebb2e8d7f5ff5a0273be5926c1601fe2b34c
Greg Wood <contact@gregwood.io>
2023-12-08 20:01:39 -0500

    Dummy book page, dummy info added into db on create

--- lib/dao/database.dart
+++ lib/dao/database.dart
@@ -34,6 +34,65 @@ class Database extends _$Database {
 
   @override
   int get schemaVersion => 1;
+
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      onCreate: (Migrator m) async {
+        await m.createAll();
+
+        // dummy data
+        await m.database.batch((batch) async {
+          batch.insert(reader, ReaderCompanion.insert(name: 'Teddy Ruxpin'));
+          batch.insert(
+            book,
+            BookCompanion.insert(
+              title: 'The Very Hungry Caterpillar (Storytime Giants)',
+              author: 'Eric Carle',
+              isbn: '9780399250453',
+            ),
+          );
+
+          // final readerRows = await select(reader).get();
+          // final bookRows = await select(book).get();
+
+          batch.insert(
+              bookRead,
+              BookReadCompanion(
+                bookId: Value<int>(1),
+                readerId: Value<int>(1),
+                timestamp: Value<String>(DateTime.now().toIso8601String()),
+              ));
+        });
+
+        // final reader = ReaderCompanion.insert(name: 'Teddy Ruxpin');
+        // final book = BookCompanion.insert(
+        //   title: 'The Very Hungry Caterpillar (Storytime Giants)',
+        //   author: 'Eric Carle',
+        //   isbn: '9780399250453',
+        // );
+        // BookReadCompanion.insert(
+        //   readerId: reader.id.value,
+        //   bookId: book.id.value,
+        //   timestamp: DateTime.now().toIso8601String(),
+        // );
+
+        // m.database.batch((batch) => batch.insertAll(reader, ReaderCompanion.insert(name: 'Teddy Ruxpin')))
+      },
+      onUpgrade: (Migrator m, int from, int to) async {
+        // if (from < 2) {
+        //   // we added the dueDate property in the change from version 1 to
+        //   // version 2
+        //   await m.addColumn(todos, todos.dueDate);
+        // }
+        // if (from < 3) {
+        //   // we added the priority property in the change from version 1 or 2
+        //   // to version 3
+        //   await m.addColumn(todos, todos.priority);
+        // }
+      },
+    );
+  }
 }
 
 final dbProvider = StateProvider<Database>((ref) => constructDb());
--- lib/pages/library_page.dart
+++ lib/pages/library_page.dart
@@ -1,12 +1,45 @@
+import 'package:a_bunch_of_books/dao/dao.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../models/models.dart';
+
 class LibraryPage extends ConsumerWidget {
+  const LibraryPage({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Center(
-        child: Text('Library'),
+      body: StreamBuilder<List<Book>>(
+        stream: ref.read(daoProvider).getBooks(),
+        builder: (BuildContext context, AsyncSnapshot<List<Book>> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Theme.of(context).primaryColor,
+              ),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error!.toString()),
+            );
+          }
+
+          final books = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: books.length,
+            itemBuilder: (context, index) {
+              final book = books[index];
+
+              return Text(book.title);
+            },
+          );
+        },
       ),
     );
   }
