e0f11ae5cef9c45b9cba9628437a52d785b29308
Greg Wood <contact@gregwood.io>
2023-12-09 18:33:12 -0500

    Reorganize actions and scaffolds for pages

--- lib/pages/library_page.dart
+++ lib/pages/library_page.dart
@@ -1,8 +1,10 @@
 import 'package:a_bunch_of_books/dao/dao.dart';
+import 'package:a_bunch_of_books/widgets/scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../models/models.dart';
+import '../widgets/book_search.dart';
 
 class LibraryPage extends ConsumerWidget {
   const LibraryPage({
@@ -11,7 +13,35 @@ class LibraryPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
+    return ABOBScaffold(
+      actions: [
+        IconButton(
+          onPressed: () {
+            showSearch(context: context, delegate: BookSearch(ref));
+          },
+          icon: const Icon(Icons.search),
+        ),
+        PopupMenuButton<String>(
+          onSelected: (String result) {
+            // Sort books
+            // if (result == "Alphabetically") {
+            //   // Sort Alphabetically
+            // } else if (result == "Last Read") {
+            //   // Sort by last read
+            // }
+          },
+          itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+            const PopupMenuItem<String>(
+              value: "Alphabetically",
+              child: Text('Sort Alphabetically'),
+            ),
+            const PopupMenuItem<String>(
+              value: "Last Read",
+              child: Text('Sort by Last Read'),
+            ),
+          ],
+        ),
+      ],
       body: StreamBuilder<List<Book>>(
         stream: ref.read(daoProvider).getBooks(),
         builder: (BuildContext context, AsyncSnapshot<List<Book>> snapshot) {
