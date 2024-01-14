9489cf5bd0645df48e9098162c207b5a56d45f26
Greg Wood <contact@gregwood.io>
2024-01-14 15:48:32 -0500

    Add GitHub link to About page

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:line_icons/line_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends ConsumerWidget {
@@ -9,67 +10,90 @@ class AboutPage extends ConsumerWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(
-              'About',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'A Bunch of Books ',
-                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                            fontWeight: FontWeight.bold,
+            SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'About',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'A Bunch of Books ',
+                            style:
+                                Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
                           ),
-                    ),
-                    TextSpan(
-                      text:
-                          'is a companion app for the \'1000 Books by Kindergarten\' challenge to promote early childhood literacy. The goal is to simply read 1000 books to your child before starting school. Yes, repeats are allowed. ',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    TextSpan(
-                      text: 'A Bunch of Books ',
-                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                            fontWeight: FontWeight.bold,
+                          TextSpan(
+                            text:
+                                'is a companion app for the \'1000 Books by Kindergarten\' challenge to promote early childhood literacy. The goal is to simply read 1000 books to your child before starting school. Yes, repeats are allowed. ',
+                            style: Theme.of(context).textTheme.bodyLarge,
                           ),
-                    ),
-                    TextSpan(
-                      text:
-                          'helps you through this challenge by letting you create a library of books your child has read and tracks their reading progress.\n',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    TextSpan(
-                      text: 'A Bunch of Books ',
-                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                            fontWeight: FontWeight.bold,
+                          TextSpan(
+                            text: 'A Bunch of Books ',
+                            style:
+                                Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
                           ),
+                          TextSpan(
+                            text:
+                                'helps you through this challenge by letting you create a library of books your child has read and tracks their reading progress.\n',
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          TextSpan(
+                            text: 'A Bunch of Books ',
+                            style:
+                                Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                          ),
+                          TextSpan(
+                            text:
+                                'is not affiliated with the 1000 Books Foundation. You can learn more about the challenge and foundation at ',
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          link(context,
+                              'https://1000booksbeforekindergarten.org/'),
+                          TextSpan(
+                            text:
+                                'No copyright infringement is intended. This is just an app made by a software developer dad.\n',
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          TextSpan(
+                            text:
+                                'This app uses Open Library as the source for books. Please consider supporting them as well at ',
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          link(context, 'https://openlibrary.org/')
+                        ],
+                      ),
                     ),
-                    TextSpan(
-                      text:
-                          'is not affiliated with the 1000 Books Foundation. You can learn more about the challenge and foundation at ',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    link(context, 'https://1000booksbeforekindergarten.org/'),
-                    TextSpan(
-                      text:
-                          'No copyright infringement is intended. This is just an app made by a software developer dad.\n',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    TextSpan(
-                      text:
-                          'This app uses Open Library as the source for books. Please consider supporting them as well at ',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    link(context, 'https://openlibrary.org/')
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: IconButton(
+                onPressed: () {
+                  try {
+                    launchUrl(Uri.parse(
+                        'https://github.com/gregwoodio/a_bunch_of_books'));
+                  } catch (_) {}
+                },
+                icon: const Icon(LineIcons.github),
+              ),
+            )
           ],
         ),
       ),
